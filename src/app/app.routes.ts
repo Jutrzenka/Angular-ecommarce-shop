16494834402278b55785b14948b36127a729ce02
Kacper Czaja <kacperczaja@duck.com>
2025-10-01 21:45:33 +0200

    Set ups routings home, cart, gift-cards and products

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Products } from './pages/products/products';
+import { GiftCards } from './pages/gift-cards/gift-cards';
+import { Cart } from './pages/cart/cart';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'products', component: Products },
+  { path: 'gift-cards', component: GiftCards },
+  { path: 'cart', component: Cart },
+  { path: '**', redirectTo: '' },
+];
---
+++ src/app/pages/gift-cards/gift-cards.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gift-cards',
+  imports: [],
+  templateUrl: './gift-cards.html',
+  styleUrl: './gift-cards.scss'
+})
+export class GiftCards {
+
+}
