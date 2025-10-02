3aa34653fcc75340628d05e86138fc8771950c30
Kacper Czaja <kacperczaja@duck.com>
2025-10-02 22:37:24 +0200

    Extracting the carousel-button component from /home

--- src/app/shared/carousel-button/carousel-button.ts
+++ src/app/shared/carousel-button/carousel-button.ts
@@ -1,4 +1,4 @@
-import { Component, input, computed, Signal, EventEmitter, Output } from '@angular/core';
+import { Component, input, computed, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,19 +9,12 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./carousel-button.scss'],
 })
 export class CarouselButtonComponent {
-  // 1. Sygnał wejściowy zamiast @Input()
   direction = input<'prev' | 'next'>();
-
-  // 2. Obliczone sygnały dla ścieżki SVG i aria-label
-  path: Signal<string> = computed(() =>
-    this.direction() === 'prev' ? 'M5 1 1 5l4 4' : 'm1 9 4-4-4-4'
-  );
-  ariaLabel: Signal<string> = computed(() =>
+  path = computed(() => (this.direction() === 'prev' ? 'M5 1 1 5l4 4' : 'm1 9 4-4-4-4'));
+  ariaLabel = computed(() =>
     this.direction() === 'prev' ? 'Poprzedni produkt' : 'Następny produkt'
   );
-
-  // 3. Zwykły Output do emiterowania kliknięcia
-  @Output() navigate = new EventEmitter<void>();
+  navigate = output<void>();
 
   onClick() {
     this.navigate.emit();
